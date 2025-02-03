65e17fa4dc0d102c34baabc0e93923739707f174
ProgrammoOra <rbambini@gmail.com>
2025-02-03 15:35:39 +0100

    Update SolarApp.py x selected_datetime

--- SolarApp.py
+++ SolarApp.py
@@ -30,6 +30,8 @@ selected_time = st.time_input("⏰ Choose a Time:", value=default_time)
 # Convert to datetime
 selected_datetime = datetime.datetime.combine(selected_date, selected_time)#, tzinfo = timezone)
 st.write(f"**Selected DateTime:** {selected_datetime}")
+selected_datetime = selected_datetime(timezone)
+st.write(f"**Selected DateTime:** {selected_datetime}")
 
 # --- Define Location ---
 latitude = st.number_input("🌍 Latitude", value=45.4642)
